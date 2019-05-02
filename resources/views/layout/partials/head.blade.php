36d4a1c5a440c7ee8a99911f30669743f81c071e
Ravi Gunasena <ravigunz@gmail.com>
2019-05-02 17:44:00 +0530

    Template created

---
+++ resources/views/layout/partials/head.blade.php
@@ -0,0 +1,25 @@
+<meta charset="utf-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<meta name="description" content="">
+
+<meta name="author" content="">
+
+<title>Album example for Bootstrap</title>
+
+<!-- Bootstrap core CSS -->
+
+<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUIyJ" crossorigin="anonymous"> -->
+
+<base href="{{ URL::asset('/') }}">
+    <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ url('css/mystyles.css') }}">
+
+    <script src="{{ url('js/jquery.min.js') }}"></script>
+    <script src="{{ url('js/myscripts.js') }}"></script>
+
+
+<!-- Custom styles for this template -->
+
+<!-- <link href="/css/album.css" rel="stylesheet"> -->
